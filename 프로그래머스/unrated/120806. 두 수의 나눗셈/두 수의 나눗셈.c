991ce9745ca31bd475735ba71e10e4065af821f9
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2023-10-28 12:03:44 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if (0 < num1 && num1 <= 100 && 0 < num2 && num2 <= 100) {
+        answer = ((float)num1 / num2) * 1000;
+    }
+    return answer;
+}
