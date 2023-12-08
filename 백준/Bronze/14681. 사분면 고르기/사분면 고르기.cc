b6f5d4e129c81335bc107e8d5692d657398d506a
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2023-12-08 22:47:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    
+    cin >> A;
+    cin >> B;
+    
+    if(A>0 && B>0)
+        cout << "1" << endl;
+    else if(A<0 && B>0)
+        cout << "2" << endl;
+    else if(A<0 && B<0)
+        cout << "3" << endl;
+    else
+        cout << "4" << endl;
+    return 0;
+}
