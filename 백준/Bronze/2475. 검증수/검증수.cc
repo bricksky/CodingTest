d4d528fa35e5ffee32fb284aed1d8a90e25dfe64
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-01-01 20:14:02 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int answer = 0;
+    
+    for(int i=1; i<=5; i++){
+        cin >> a;
+        b += a*a;
+    }
+    answer = b % 10;
+    cout << answer << '\n';
+}
