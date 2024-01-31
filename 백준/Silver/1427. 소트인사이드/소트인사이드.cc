d0eb5dc14fed302684ad1ce106d1f1fa70dff286
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-01-31 19:20:47 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    
+    sort(str.begin(), str.end(), greater<char>());
+    cout << str;
+}
