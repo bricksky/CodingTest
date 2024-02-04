02b1b447c3ddd46881bd73986a184a05fd96e595
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-02-04 12:09:19 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int n = 0;
+
+int main()
+{
+    int sum=0;
+    
+    while(true){
+        cin >> n;
+        if(n == -1)
+            break;
+        sum += n;
+    }
+    cout << sum << '\n';
+}
