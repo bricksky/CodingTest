a44f40dc52135e6633f0c58e800cde5f11d9bcea
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-02-09 20:36:56 +0900

    [Bronze II] Title: 피시방 알바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 0, customer, num, a[101] = {0};
+    cin >> customer;
+    
+    for(int i=0; i<customer; i++){
+        cin >> num;
+        if(a[num] > 0)count++;
+        a[num]++;
+    }
+    cout << count << '\n';
+}
