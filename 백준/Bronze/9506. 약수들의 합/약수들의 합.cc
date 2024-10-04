a792dc3781ce832027bd54fb63e13f4f86cb90d7
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-10-04 22:45:04 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 2096 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    
+    while (true)
+    {
+        cin >> n;
+
+        // 종료되는 조건
+        if(n == -1)
+            break;
+        
+        int sum = 0;
+        int t = 0;
+        int arr[50'000];
+
+        // 약수 구하는 부분 (n 자신을 제외)
+        for(int i = 1; i < n; i++){
+            if(n % i == 0){
+                arr[t++] = i;
+                sum += i;
+            }
+        }
+
+        // 완전수 여부 판단하는 부분
+        if(sum == n){
+            cout << n << " = ";
+            for(int i = 0; i < t; i++){
+                if(i == t - 1){
+                    cout << arr[i]; 
+                } else {
+                    cout << arr[i] << " + "; 
+                }
+            }
+            cout << endl;
+        } else {
+            cout << n << " is NOT perfect." << endl; 
+        }
+    }
+
+    return 0;
+}
