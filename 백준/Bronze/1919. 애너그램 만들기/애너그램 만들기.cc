f572c49b6e8b88d68b60a6ac4e1a57f5e09d71d6
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-10-13 16:10:22 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main(){
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int alphabet1[26] = {};
+    int alphabet2[26] = {};
+
+    for(int i=0; i<s1.length(); i++){
+        alphabet1[s1[i] - 'a']++;
+    }
+
+    for(int i=0; i<s2.length(); i++){
+        alphabet2[s2[i] - 'a']++;
+    }
+
+    int result = 0;
+    for(int i=0; i<26; i++){
+        if(alphabet1[i] != alphabet2[i]){
+            result += abs(alphabet1[i] - alphabet2[i]);
+        }
+    }
+    cout << result;
+    return 0;
+}
