8746aed65c7eea557d080616d64227cc5d7a9e9d
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2024-11-09 16:22:31 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1532 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int count[10001] = {0};
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        count[num]++;
+    }
+
+    for (int i = 1; i <= 10000; i++) {
+        while (count[i] > 0) {
+            cout << i << '\n';
+            count[i]--;
+        }
+    }
+
+    return 0;
+}
