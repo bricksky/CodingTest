bd6c5ec019bbe23424b6d82b092fe810f99e8941
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2025-01-23 15:37:51 +0900

    [Bronze I] Title: N번째 큰 수, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int t;
+    int arr[10];
+
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+
+        // 각 테스트 케이스마다 10개의 숫자를 입력받음
+        for(int j=0; j<10; j++){
+            cin >> arr[j];
+        }
+        
+        // 내림차순으로 정렬
+        sort(arr, arr+10, greater<int>());
+        cout << arr[2] << endl;
+    }
+    return 0;
+}
