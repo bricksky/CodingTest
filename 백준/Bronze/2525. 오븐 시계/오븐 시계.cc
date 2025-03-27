be21cfccb87a46ad60b3243c27a5e66b5a589a48
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2025-03-27 20:22:40 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -1,22 +1,17 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int A, B;
-    cin >> A >> B;
+int main(){
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    int total = a*60 + b + c;
     
-    int C;
-    cin >> C;
+    a = (total / 60) % 24;
+    b = total % 60;
 
-    B += C; // 먼저 분을 더함
-    A += B / 60; // 시간에 분을 시간으로 변환하여 더함
-    B %= 60; // 분을 60으로 나눈 나머지로 설정
-
-    if (A >= 24) {
-        A -= 24; // 만약 시간이 24 이상이면, 24를 빼서 0시부터 다시 시작하도록 함
-    }
-
-    cout << A << " " << B;
+    cout << a << " " << b;
     return 0;
 }
