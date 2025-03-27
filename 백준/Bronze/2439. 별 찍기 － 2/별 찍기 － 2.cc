40e85b42d7391f35a3b007e90921b50e6fe82ea2
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2025-03-27 20:52:17 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -1,22 +1,19 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int N = 0;
+int main(){
+    int N;
     cin >> N;
+
     for(int i=1; i<=N; i++){
-        for(int k=0; k<N-i; k++){
-            cout << ' ';
+        for(int j=0; j<N-i; j++){
+            cout << " ";
         }
-        for(int j=1; j<=i; j++)
-        {
-            cout << '*';
+        for(int j=0; j<i; j++){
+            cout << "*";
         }
-        cout << '\n';
+        cout << endl;
     }
     return 0;
 }
