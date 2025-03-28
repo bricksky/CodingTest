54ea978c2022b1a5036adb2d1e200aaf9ddf0150
DongHyun Kim <113027703+bricksky@users.noreply.github.com>
2025-03-28 22:46:58 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N;
+    int count = 0;
+    map <string, int> m;
+    
+    cin >> N;
+    while(N--){
+        string bookName;
+        cin >> bookName;    
+        m[bookName]++;
+    }
+
+    for(auto A : m) count = max(count, A.second);
+    for(auto A : m){
+        if(A.second == count){
+            cout << A.first;
+            return 0;
+        }
+    }
+}
